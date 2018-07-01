b3c004d1dae18cc717ba4f4b188b78d77ce43870
Scott Grobe <31743193+Git-Grobe@users.noreply.github.com>
2018-07-01 18:31:21 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("clear")
+
+os.system("say -v samantha -r 150 --interactive=red Hello Stephen. Hello World!")
+
+os.system("say -v fred -r 150  --interactive=red Hello Siri. Hello World!")
